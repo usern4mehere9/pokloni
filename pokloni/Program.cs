010342d8836c691c88f283cbe0452890fc7e47b5
usern4mehere9 <91462628+usern4mehere9@users.noreply.github.com>
2021-09-29 09:42:35 +0200

    Add files via upload

---
+++ pokloni/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace pokloni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int k, n;
+
+            k = int.Parse(Console.ReadLine());
+            n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(n / k);
+
+        }
+    }
+}
